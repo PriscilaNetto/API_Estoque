c5f731072fdd948aea97cfa3468fc19f2dc5e279
Priscila Netto <priscila.nnetto@hotmail.com>
2019-11-28 15:36:25 -0300

    inclusao da const cors e body parser

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const index = require('./routes/index')
+const cors = require('cors')
 
-const app = express();
+const bodyParser = require('body-parser')
 
-const index = require('./routes/index');
 
-// const listaProdutos = require('./routes/produtosRoute');
 
-app.use(function (request, response, next){
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header(
-        'Acess-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next()
-});
+// const listaProdutos = require('./routes/produtos');
 
-app.use('/', index);
-// app.use('produtos', produtos);
+app.use(bodyParser.json())
+app.use(cors())
+
+// app.use(function (request, response, next){
+//     response.header('Access-Control-Allow-Origin', '*')
+//     response.header(
+//         'Acess-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     next()
+// });
+app.use('/', index)
+// app.use('produtos', produtos)
 
 module.exports = app
