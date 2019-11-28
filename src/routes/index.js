67082c48ae19e4e4243965f946d75840f986b0be
Priscila Netto <priscila.nnetto@hotmail.com>
2019-11-28 15:36:49 -0300

    alteração do title

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', function (request, response){
     response.status(200).send({
-        title: "Estoque da Manifesto Singular, olarrr",
+        title: "Estoque da Manifesto Singular",
         version: "1.0.0"
     })
 })
