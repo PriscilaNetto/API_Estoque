975a6c2049fef2ce0ae4389b7c28ae9584bda462
Priscila Netto <priscila.nnetto@hotmail.com>
2019-11-28 15:37:21 -0300

    criação do file produtos e rotas

---
+++ src/routes/produtos.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const router = express.Router()
+const controller = require("../controllers/produtosController")
+
+router.get('', controller.listaProdutos)
+router.post('', controller.novoProduto)
+router.get('/:id', controller.buscaId)
+router.patch('/:id', controller.atualizaProduto)
+router.delete('/:id', controller.removeProduto)
+
+module.exports = router
