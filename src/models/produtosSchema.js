e03a765c49c518b60091248f343792e3d25f2b3b
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-08 12:59:23 -0300

    criação do  produtosSchema

---
+++ src/models/produtosSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const produtosSchema = new Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
+    nome: { type: String, required: true},
+    tamanho: { type: String, required: true},
+    modelagem: { type: String, required: true},
+    cor: { type: String, required: true},
+    quantidade: { type: String, required: true},
+    fornecedor: { type: String, required: true}
+})
+
+const produtosCollection = mongoose.model('produtos', produtosSchema)
+
+module.exports = {produtosCollection}
