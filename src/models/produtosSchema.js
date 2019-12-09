299605f3ecb7eccf4bc46aaf8eb0122554dd7aa9
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-09 11:26:32 -0300

    alteração do campo quant. para Number

--- src/models/produtosSchema.js
+++ src/models/produtosSchema.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const produtosSchema = new Schema({
+const ProdutosSchema = new Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
     nome: { type: String, required: true},
     tamanho: { type: String, required: true},
     modelagem: { type: String, required: true},
     cor: { type: String, required: true},
-    quantidade: { type: String, required: true},
+    quantidade: { type: Number, required: true},
     fornecedor: { type: String, required: true},
     dataEntrada: {type: Date},
     dataSaída: {type: Date}
 })
 
-const produtosCollection = mongoose.model('produtos', produtosSchema)
+const produtosModel = mongoose.model('produtos', ProdutosSchema)
 
-module.exports = {produtosCollection}
+module.exports = produtosModel
