2f6f65863d3f4d5eef730bf08ada4e99ca0658a6
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-10 19:38:01 -0300

    inclusão da rota delete

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -6,6 +6,6 @@ router.get('', controller.listaProdutos)
 router.post('', controller.novoProduto)
 router.get('/:id', controller.produtoById)
 router.patch('/:id', controller.atualizaProduto)
-// router.delete('/:id', controller.removeProduto)
+router.delete('/:id', controller.removeProduto)
 
 module.exports = router
