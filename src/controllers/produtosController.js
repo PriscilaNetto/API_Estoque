5c52b2202e2f55f82d2cc49a1563a55f98520e62
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-10 19:38:30 -0300

    inclusão do update e delete

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -40,9 +40,44 @@ const produtoById = (request, response) => {
     })
 }
 
+const atualizaProduto = (request, response) => {
+    const id = request.params.id
+    const alterarProduto = request.body
+    const options = {new: true}
+
+    produtosModel.findByIdAndUpdate(
+        id, alterarProduto, options,
+        (error, produto) => {
+            if(error) {
+                return response.status(500).send(error)
+            }
+            if(produto) {
+                return response.status(200).send(produto)
+            }
+            return response.status(404).send('Xii, não encontrei seu produto')
+        }
+    )
+}
+
+const removeProduto = (request, response) => {
+    const id  = request.params.id
+    produtosModel.findByIdAndRemove(id, (error, produto) => {
+        if (error) {
+            return response.status(500).send(error)
+        }
+
+        if (produto) {
+            return response.status(200).send(id)
+        }
+            return response.status(404).send('Xii, não encontrei seu produto')
+    })
+}
+
 
 module.exports = {
     listaProdutos,
     novoProduto,
-    produtoById
+    produtoById,
+    atualizaProduto,
+    removeProduto
 }
