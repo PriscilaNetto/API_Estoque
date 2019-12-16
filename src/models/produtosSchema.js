822c994a45f84b20e18555384cb2f26af46de9db
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-16 20:10:12 -0300

    inclusão da chave quantidadeVenda

--- src/models/produtosSchema.js
+++ src/models/produtosSchema.js
@@ -7,6 +7,7 @@ const ProdutosSchema = new Schema({
     modelagem: { type: String, required: true},
     cor: { type: String, required: true},
     quantidade: { type: Number, required: true},
+    quantidadeVenda: {type: Number},
     fornecedor: { type: String, required: true},
     dataEntrada: {type: Date},
     dataSaída: {type: Date}
