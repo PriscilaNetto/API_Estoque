99bebee808dc2a399b4e01f51ef0704961da1cbf
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-16 20:11:32 -0300

    criação da rota atualizaQuantidade

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -6,6 +6,7 @@ router.get('', controller.listaProdutos)
 router.post('', controller.novoProduto)
 router.get('/:id', controller.produtoById)
 router.patch('/:id', controller.atualizaProduto)
+router.patch('/:id', controller.atualizaQuantidade)
 router.delete('/:id', controller.removeProduto)
 
 module.exports = router
