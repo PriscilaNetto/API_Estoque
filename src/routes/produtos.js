598e460834be4596c4184c0ab64c30920cae1133
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-16 22:28:43 -0300

    alteração na rota produtoId

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/produtosController")
 
 router.get('', controller.listaProdutos)
 router.post('', controller.novoProduto)
-router.get('/id', controller.produtoById)
+router.get('/:id', controller.produtoById)
 router.patch('/:id', controller.atualizaProduto)
 router.delete('/:id', controller.removeProduto)
 
