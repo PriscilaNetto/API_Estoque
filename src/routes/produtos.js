38923283dcb251d7437ed44a30e37284626e82bc
Priscila Netto <priscila.nnetto@hotmail.com>
2019-12-16 21:51:33 -0300

    alteração de rota

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -4,9 +4,8 @@ const controller = require("../controllers/produtosController")
 
 router.get('', controller.listaProdutos)
 router.post('', controller.novoProduto)
-router.get('/:id', controller.produtoById)
+router.get('/id', controller.produtoById)
 router.patch('/:id', controller.atualizaProduto)
-// router.patch('/:id', controller.atualizaQuantidade)
 router.delete('/:id', controller.removeProduto)
 
 module.exports = router
