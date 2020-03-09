48e906bcb60ca137e3322a52decbf3d4c3ee1d1e
Priscila Netto <priscila.nnetto@hotmail.com>
2020-03-09 18:41:26 -0300

    correções na pasta app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-require('dotenv-safe').load();
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const produtos = require('./src/routes/produtos')
-const database = require('./src/models/database')
-database.connect()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv-safe").load();
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use('/produtos', produtos)
+const produtos = require("./src/routes/produtos");
+const database = require("./src/models/database");
+database.connect();
 
-const port = process.env.PORT || 3001
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/produtos", produtos);
 
-app.get('/', (request, response) => {
-    response.send('E aí, turo booom?')
-})
+const port = process.env.PORT || 3001;
 
-    app.listen(port, function() {
-    console.info(`toc, toc na porta ${port}`);
-})
+app.get("/", (request, response) => {
+  response.send("E aí, turo booom?");
+});
 
+app.listen(port, function() {
+  console.info(`toc, toc na porta ${port}`);
+});
 
-module.exports = app
+module.exports = app;
